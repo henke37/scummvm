c8220092cac50dbae51eab7dc78e1d5df4e3defe
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-11-23 09:51:19 +0100

    Include the compStream.cpp file in the build

--- backends/module.mk
+++ backends/module.mk
@@ -203,6 +203,7 @@ MODULE_OBJS += \
 	fs/windows/windows-fs.o \
 	fs/windows/windows-fs-factory.o \
 	fs/windows/windows-iostream.o \
+	fs/compStream.o \
 	midi/windows.o \
 	plugins/win32/win32-provider.o \
 	saves/windows/windows-saves.o \
