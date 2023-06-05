e8dfec530943f1a9a77e76861077c7252d414980
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2023-06-05 17:00:08 +0200

    ANDROID: Initial bitmap conversion code for printing

--- backends/printing/android/android-printman.cpp
+++ backends/printing/android/android-printman.cpp
@@ -18,6 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+#define FORBIDDEN_SYMBOL_ALLOW_ALL
  
 #include "backends/printing/printman.h"
 
@@ -28,6 +29,14 @@
 
 #include "android-printman.h"
 #include "common/ustr.h"
+
+
+#include <android/bitmap.h>
+
+#include "backends/platform/android/jni-android.h"
+
+#include "common/error.h"
+#include "graphics/managed_surface.h"
 
 class AndroidPrintJob;
 class AndroidPrintSettings;
@@ -95,6 +104,8 @@ private:
 
 AndroidPrintingManager::~AndroidPrintingManager() {}
 
+jobject surf2Bitmap(const Graphics::ManagedSurface &surf);
+
 PrintJob *AndroidPrintingManager::createJob(const Common::String &jobName, PrintSettings *settings) {
 	return new AndroidPrintJob(jobName, (AndroidPrintSettings*)settings);
 }
@@ -108,9 +119,19 @@ AndroidPrintJob::~AndroidPrintJob() {
 }
 
 void AndroidPrintJob::drawBitmap(const Graphics::ManagedSurface &surf, Common::Point pos) {
+	JNIEnv *env = JNI::getEnv();
+	
+	jobject bitmapObj = surf2Bitmap(surf);
+	
+	env->DeleteLocalRef(bitmapObj);
 }
 
 void AndroidPrintJob::drawBitmap(const Graphics::ManagedSurface &surf, Common::Rect posAndSize) {
+	JNIEnv *env = JNI::getEnv();
+	
+	jobject bitmapObj = surf2Bitmap(surf);
+	
+	env->DeleteLocalRef(bitmapObj);
 }
 
 void AndroidPrintJob::drawText(const Common::String &text, Common::Point pos) {
@@ -194,6 +215,103 @@ bool AndroidPrintSettings::getColorPrinting() const {
 }
 
 void AndroidPrintSettings::setColorPrinting(bool colorPrinting) {
+}
+
+jobject surf2Bitmap(const Graphics::ManagedSurface &srcSurf) {
+	JNIEnv *env = JNI::getEnv();
+	
+	jclass bitmapClazz = env->FindClass("android/graphics/Bitmap");
+	if(!bitmapClazz) {
+		error("Failed to FindClass(Bitmap)");
+		return nullptr;
+	}
+	
+	jmethodID MID_bitmapCreateBitmap = env->GetStaticMethodID(bitmapClazz,
+		"createBitmap",
+		"(IILandroid/graphics/Bitmap$Config;)Landroid/graphics/Bitmap;"
+	);	
+	if(!MID_bitmapCreateBitmap) {
+		error("Failed to GetStaticMethodId(createBitmap)");
+		return nullptr;
+	}
+	
+	jclass bitmapConfigClass = env->FindClass("android/graphics/Bitmap$Config");
+    jfieldID FID_ARGB888 = env->GetStaticFieldID(bitmapConfigClass, "ARGB_8888", "Landroid/graphics/Bitmap$Config;");
+    
+	if(!FID_ARGB888) {
+		error("Failed to GetStaticFieldID(ARGB_8888)");
+		return nullptr;
+	}
+	jobject argb8888Config = env->GetStaticObjectField(bitmapConfigClass, FID_ARGB888);
+	if(!argb8888Config) {
+		error("Failed to GetStaticObjectField(ARGB_8888)");
+		return nullptr;
+	}
+	
+	jobject bitmapObj = env->CallStaticObjectMethod(
+		bitmapClazz,
+		MID_bitmapCreateBitmap,
+		srcSurf.w,
+		srcSurf.h,
+		argb8888Config
+	);
+	
+	env->DeleteLocalRef(argb8888Config);
+	
+	if(!bitmapObj) {
+		error("Failed to create Bitmap object");
+		return nullptr;
+	}
+
+	AndroidBitmapInfo bitmap_info;
+	if (AndroidBitmap_getInfo(env, bitmapObj, &bitmap_info) != ANDROID_BITMAP_RESULT_SUCCESS) {
+		error("Error reading bitmap info");
+		env->DeleteLocalRef(bitmapObj);
+		return nullptr;
+	}
+	
+	Graphics::PixelFormat dstFmt;
+	switch(bitmap_info.format) {
+		case ANDROID_BITMAP_FORMAT_RGBA_8888:
+#ifdef SCUMM_BIG_ENDIAN
+			dstFmt = Graphics::PixelFormat(4, 8, 8, 8, 8, 24, 16, 8, 0);
+#else
+			dstFmt = Graphics::PixelFormat(4, 8, 8, 8, 8, 0, 8, 16, 24);
+#endif
+			break;
+		case ANDROID_BITMAP_FORMAT_RGBA_4444:
+			dstFmt = Graphics::PixelFormat(2, 4, 4, 4, 4, 12, 8, 4, 0);
+			break;
+		case ANDROID_BITMAP_FORMAT_RGB_565:
+			dstFmt = Graphics::PixelFormat(2, 5, 6, 5, 0, 11, 5, 0, 0);
+			break;
+		default:
+			error("Bitmap has unsupported format");
+			env->DeleteLocalRef(bitmapObj);
+			return nullptr;
+	}
+	
+	{
+		Graphics::ManagedSurface dstSurf = Graphics::ManagedSurface();
+
+		void *dst_pixels = nullptr;
+		if (AndroidBitmap_lockPixels(env, bitmapObj, &dst_pixels) != ANDROID_BITMAP_RESULT_SUCCESS) {
+			error("Error locking bitmap pixels");
+			env->DeleteLocalRef(bitmapObj);
+			return nullptr;
+		}
+		dstSurf.w=bitmap_info.width;
+		dstSurf.h=bitmap_info.height;
+		dstSurf.pitch=bitmap_info.stride;
+		dstSurf.format=dstFmt;
+		dstSurf.setPixels(dst_pixels);
+		
+		dstSurf.blitFrom(srcSurf);
+
+		AndroidBitmap_unlockPixels(env, bitmapObj);
+	}
+	
+	return bitmapObj;
 }
 
 
